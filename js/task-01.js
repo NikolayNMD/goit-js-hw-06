6898f4a97b6ce722a5b3979b97c71bc542815b95
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-09-21 21:12:35 +0300

    1st commit

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+// Number of categories: 3
+const categories = document.querySelectorAll('.item');
+console.log('Number of categories: ' + categories.length);
+
+// Category: Animals
+// Elements: 4
+
+categories.forEach(element => {
+    // get h2
+    // get li count
+    console.log('Category: ' + 'sdadas' + 'Elements: ' + 232)
+});

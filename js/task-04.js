602845ba3330871880a274c011d7f0cb47e8b5c0
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-09-26 23:11:59 +0300

    done task 4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+const valueToGet = document.getElementById('value');
+
+const buttons = document.getElementsByTagName('button');
+
+for (let i = 0; i < buttons.length; i++) {
+    let button = buttons[i];
+    button.addEventListener('click', function () {
+        if (this.getAttribute('data-action') === 'decrement') {
+            counterValue -= 1;
+        } else {
+            counterValue += 1;
+        }
+        valueToGet.textContent = counterValue;
+    });
+}
